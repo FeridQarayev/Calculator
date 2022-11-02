9eb80f941974255d1c0b38503cd5ca4d133d1153
FeridQarayev <tu7he6n9s@code.edu.az>
2022-11-02 16:30:42 +0400

    Add changes

--- FeridApp/Program.cs
+++ FeridApp/Program.cs
@@ -1,25 +1,34 @@
 ﻿
 
 Console.WriteLine("\t\t \t \t \t \t Welcome to blank");
+
+string username = "nobody";
+string password = "O555";
+
+int totalAmount = 0;
+int amount = 0;
+int totalMonth = 0;
+int monthCount = 0;
+int percent = 0;
+int payAmount = 0;
+int monthlyPayment = 0;
+int proccesConsole = 0;
+int authorize = 0;
+string usernameConsole = "";
+string passwordConsole = "";
 while (true)
 {
-    string username = "nobody";
-    string password = "O555";
-
-    int totalAmount = 0;
-    int monthCount = 0;
-    int percent = 0;
-    int payAmount = 0;
-    int monthlyPayment = 0;
-    Console.WriteLine("Enter username :");
-    string usernameConsole = Console.ReadLine();
-    Console.WriteLine("Enter password :");
-    string passwordConsole = Console.ReadLine();
-    int proccesConsole = 0;
+    if (authorize == 0)
+    {
+        Console.WriteLine("Enter username :");
+        usernameConsole = Console.ReadLine();
+        Console.WriteLine("Enter password :");
+        passwordConsole = Console.ReadLine();
+    }
 
     if (usernameConsole == username && passwordConsole == password)
     {
-
+        authorize = 1;
         if (proccesConsole == 0)
         {
             Console.WriteLine("\t\t \t \t \t \t Welcome Nobody");
@@ -36,78 +45,109 @@ while (true)
                     Console.WriteLine($"Emeliyyat : {proccesConsole}");
                     Console.Write($"Ay daxiledin:");
                     monthCount = int.Parse(Console.ReadLine());
-                    Console.Write("Meblegi daxil edin : ");
-                    payAmount = int.Parse(Console.ReadLine());
-                    if (monthCount > 12)
+                    if (monthCount>0)
                     {
-                        percent = payAmount * 18 / 100;
-                        totalAmount = payAmount + percent;
-
-                        monthlyPayment = totalAmount / monthCount;
-
-                        Console.WriteLine($"Odenilecek mebleq : {totalAmount}AZN");
-                        Console.WriteLine($"Ayliq Odenis : {monthlyPayment}AZN");
-                        Console.WriteLine("Razisiniz ?");
-                        Console.WriteLine("1 : Beli");
-                        Console.WriteLine("2 : Xeyr");
-                        int proccesConsole2 = int.Parse(Console.ReadLine());
-                        if (proccesConsole2 == 1)
+                        Console.Write("Meblegi daxil edin : ");
+                        payAmount = int.Parse(Console.ReadLine());
+                        if (payAmount>0)
                         {
-                            Console.WriteLine("Emeliyyat ugurlu oldu yene gelin :)");
-                            Console.WriteLine($"Indiki borcunuz : {totalAmount}");
+                            if (monthCount > 12)
+                            {
+                                percent = payAmount * 18 / 100;
+                                amount = payAmount + percent;
+
+                                monthlyPayment = amount / monthCount;
+
+                                Console.WriteLine($"Odenilecek mebleq : {amount}AZN");
+                                Console.WriteLine($"Ayliq Odenis : {monthlyPayment}AZN");
+                                Console.WriteLine("Razisiniz ?");
+                                Console.WriteLine("1 : Beli");
+                                Console.WriteLine("2 : Xeyr");
+                                int proccesConsole2 = int.Parse(Console.ReadLine());
+                                if (proccesConsole2 == 1)
+                                {
+                                    totalAmount = totalAmount+amount;
+                                    totalMonth=totalMonth+monthCount;
+                                    Console.WriteLine("Emeliyyat ugurlu oldu yene gelin :)");
+                                    Console.WriteLine($"Indiki borcunuz : {totalAmount}");
+                                }
+                                else
+                                {
+                                    percent = 0;
+                                    amount = 0;
+                                    monthlyPayment = 0;
+                                    Console.WriteLine("Emeliyyat ugursuz oldu bide gelmeyin! :(");
+                                }
+                            }
+                            else
+                            {
+                                percent = payAmount * 12 / 100;
+                                amount = payAmount + percent;
+
+                                monthlyPayment = amount / monthCount;
+
+                                Console.WriteLine($"Odenilecek mebleq : {amount}AZN");
+                                Console.WriteLine($"Ayliq Odenis : {monthlyPayment}AZN");
+                                Console.WriteLine("Razisiniz ?");
+                                Console.WriteLine("1 : Beli");
+                                Console.WriteLine("2 : Xeyr");
+                                int proccesConsole2 = int.Parse(Console.ReadLine());
+                                if (proccesConsole2 == 1)
+                                {
+                                    totalAmount = totalAmount + amount;
+                                    totalMonth = totalMonth + monthCount;
+                                    Console.WriteLine("Emeliyyat ugurlu oldu yene gelin :)");
+                                    Console.WriteLine($"Indiki borcunuz : {totalAmount}");
+                                }
+                                else
+                                {
+                                    percent = 0;
+                                    amount = 0;
+                                    monthlyPayment = 0;
+                                    Console.WriteLine("Emeliyyat ugursuz oldu bide gelmeyin! :(");
+                                }
+                            }
                         }
-                        else
-                        {
-                            Console.WriteLine("Emeliyyat ugursuz oldu bide gelmeyin! :(");
-                        }
+                        else Console.WriteLine("Mebleq 0 dan boyuk olmalidi!");
                     }
                     else
                     {
-                        percent = payAmount * 12 / 100;
-                        totalAmount = payAmount + percent;
-
-                        monthlyPayment = totalAmount / monthCount;
-
-                        Console.WriteLine($"Odenilecek mebleq : {totalAmount}AZN");
-                        Console.WriteLine($"Ayliq Odenis : {monthlyPayment}AZN");
-                        Console.WriteLine("Razisiniz ?");
-                        Console.WriteLine("1 : Beli");
-                        Console.WriteLine("2 : Xeyr");
-                        int proccesConsole2 = int.Parse(Console.ReadLine());
-                        if (proccesConsole2 == 1)
-                        {
-                            Console.WriteLine("Emeliyyat ugurlu oldu yene gelin :)");
-                            Console.WriteLine($"Indiki borcunuz : {totalAmount}");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Emeliyyat ugursuz oldu bide gelmeyin! :(");
-                        }
+                        Console.WriteLine("Ay 0 dan boyuk olmalidi!");
                     }
                     break;
                 case 2:
-                    Console.WriteLine($"Emeliyyat : {proccesConsole}");
-                    Console.WriteLine($"Ayliq odenis {monthlyPayment}AZN");
-                    Console.WriteLine("Odenis etmek isteyirsizse 1 basin");
-                    int proccesConsole3 = int.Parse(Console.ReadLine());
-                    if (proccesConsole3 == 1)
+                    if (totalAmount!=0)
                     {
-                        totalAmount = totalAmount - monthlyPayment;
-                        monthCount = monthCount - 1;
-                        Console.WriteLine("Odenis ugurlu oldu :)");
-                        Console.WriteLine($"Qalan Borc : {totalAmount}");
-                        Console.WriteLine($"Qalan Ay : {monthCount}");
+                        Console.WriteLine($"Emeliyyat : {proccesConsole}");
+                        Console.WriteLine($"Ayliq odenis {monthlyPayment}AZN");
+                        Console.WriteLine("Odenis etmek isteyirsizse 1 basin");
+                        int proccesConsole3 = int.Parse(Console.ReadLine());
+                        if (proccesConsole3 == 1)
+                        {
+                            monthlyPayment = totalAmount / totalMonth;
+                            totalAmount = totalAmount - monthlyPayment;
+                            totalMonth = totalMonth - 1;
+                            Console.WriteLine("Odenis ugurlu oldu :)");
+                            Console.WriteLine($"Qalan Borc : {totalAmount}");
+                            Console.WriteLine($"Qalan Ay : {totalMonth}");
+                        }
+                        else
+                        {
+                            monthlyPayment = 0;
+                            Console.WriteLine("Emeliyyat ugursuz oldu bide gelmeyin! :(");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Emeliyyat ugursuz oldu bide gelmeyin! :(");
+
+                        Console.WriteLine("Borcunuz yoxdu");
                     }
 
                     break;
                 case 3:
                     Console.WriteLine($"Emeliyyat : {proccesConsole}");
                     Console.WriteLine($"Qalan Borc : {totalAmount}");
-                    Console.WriteLine($"Qalan Ay : {monthCount}");
+                    Console.WriteLine($"Qalan Ay : {totalMonth}");
                     break;
                 default:
                     Console.WriteLine($"Bele emeliyyat yoxdu!;");
@@ -116,6 +156,7 @@ while (true)
             Console.WriteLine($"Menu getmek ucun 0 basin;");
             proccesConsole = int.Parse(Console.ReadLine());
         }
+
 
     }
     else
